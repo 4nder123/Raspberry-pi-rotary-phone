52c0cb7d946347297346c357f76b7dcdcaedcbb6
4nder123 <57728624+4nder123@users.noreply.github.com>
2023-07-22 15:00:23 +0300

    Add files via upload

--- routing.py
+++ routing.py
@@ -5,11 +5,14 @@ class audio_route:
         self.aplay_sco = None
         self.aplay_mic = None
         self.arec_mic = None
+        self.bluealsa_aplay = "/usr/bin/bluealsa-aplay"
+        self.aplay = "/usr/bin/aplay"
+        self.arecord = "/usr/bin/arecord"
         self.device_id = "88:9F:6F:22:BE:55"
     
     def on_call_start(self):
         if self.aplay_sco is None:
-            self.aplay_sco = Popen(["bluealsa-aplay --profile-sco"], stdout=PIPE, stderr=PIPE, shell=False)
+            self.aplay_sco = Popen([self.bluealsa_aplay, "--profile-sco"], stdout=PIPE, stderr=PIPE, shell=False)
 
         # Terminate aplay and arecord if already running
         if self.aplay_mic is not None:
@@ -23,5 +26,5 @@ class audio_route:
             self.arec_mic = None
 
         # Pipe Arecord output to Aplay to send over the SCO link
-        self.arec_mic = Popen(["arecord -D plughw:1 -f S16_LE -c 1 -r 16000 mic.wav"], stdout=PIPE, shell=False)
-        self.aplay_mic = Popen(["aplay -D bluealsa:SRV=org.bluealsa,DEV=88:9F:6F:22:BE:55,PROFILE=sco mic.wav"], stdout=PIPE, stdin=self.arec_mic.stdout, shell=False)
+        self.arec_mic = Popen([self.arecord, "-D plughw:1 -f S16_LE -c 1 -r 16000 mic.wav"], stdout=PIPE, shell=False)
+        self.aplay_mic = Popen([self.aplay, "-D bluealsa:SRV=org.bluealsa,DEV=88:9F:6F:22:BE:55,PROFILE=sco mic.wav"], stdout=PIPE, stdin=self.arec_mic.stdout, shell=False)

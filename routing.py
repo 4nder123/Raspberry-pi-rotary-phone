23b06aa55e8479c8d360ceda211ddca4e7c8e915
4nder123 <57728624+4nder123@users.noreply.github.com>
2023-07-22 18:55:26 +0300

    Add files via upload

--- routing.py
+++ routing.py
@@ -33,7 +33,6 @@ class audio_route:
         if subprocess.run(['amixer', '-D', 'bluealsa', 'cset', 'numid=' + str(numid), value + "%"], capture_output=True).returncode != 0:
             print("Nonzero exit code while setting " + type + " volume")
 
-
     def on_call_start(self):
         if self.aplay_sco is None:
             self.aplay_sco = Popen([self.bluealsa_aplay,"--profile-sco"], stdout=PIPE, stderr=PIPE, shell=False)
@@ -43,28 +42,13 @@ class audio_route:
             self.aplay_mic.terminate()
             self.aplay_mic.wait()
             self.aplay_mic = None
-            
+
         if self.arec_mic is not None:
             self.arec_mic.terminate()
             self.arec_mic.wait()
             self.arec_mic = None
-    
+
         # Pipe Arecord output to Aplay to send over the SCO link
         self.arec_mic = Popen([self.arecord,"-D","plughw:1", "-f", "S16_LE", "-c", "1", "-r", "16000", "mic.wav"], stdout=PIPE, shell=False)
+        sleep(0.2)
         self.aplay_mic = Popen([self.aplay,"-D", "bluealsa:SRV=org.bluealsa,DEV="+self.device_id+",PROFILE=sco", "mic.wav"], stdout=PIPE, stdin=self.arec_mic.stdout, shell=False)
-        
-     def on_call_stop(self):
-        if self.aplay_mic is not None:
-            self.aplay_mic.terminate()
-            self.aplay_mic.wait()
-            self.aplay_mic = None
-
-        if self.arec_mic is not None:
-            self.arec_mic.terminate()
-            self.arec_mic.kill()
-            self.arec_mic = None
-
-        if self.aplay_sco is not None:
-            self.aplay_sco.terminate()
-            self.aplay_sco.wait()
-            self.aplay_sco = None

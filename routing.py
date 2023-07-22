57afcb99cd5436641c506de32829b451f4a94490
4nder123 <57728624+4nder123@users.noreply.github.com>
2023-07-22 17:31:13 +0300

    Add files via upload

--- routing.py
+++ routing.py
@@ -12,13 +12,13 @@ class audio_route:
         self.aplay = "/usr/bin/aplay"
         self.arecord = "/usr/bin/arecord"
         self.device_id = "88:9F:6F:22:BE:55"
-        
+        """
         self.bus = dbus.SystemBus()
         self.bus.add_signal_receiver(
             self._on_bluealsa_pcm_added,
             bus_name='org.bluealsa',
             signal_name='PCMAdded'
-        )
+        )"""
         
     def _on_bluealsa_pcm_added(self, path, properties):
         self.set_volumes()
@@ -26,7 +26,7 @@ class audio_route:
     def set_volumes(self):
         # Set the SCO volumes
         self._set_bluealsa_volume("SCO playback", 6, "100")
-        self._set_bluealsa_volume("SCO capture", 4, "100")
+        self._set_bluealsa_volume("SCO capture", 8, "100")
         
     def _set_bluealsa_volume(self, type, numid, value):
         if subprocess.run(['amixer', '-D', 'bluealsa', 'cset', 'numid=' + str(numid), value + "%"], capture_output=True).returncode != 0:
@@ -35,7 +35,7 @@ class audio_route:
 
     def on_call_start(self):
         if self.aplay_sco is None:
-            self.aplay_sco = Popen([self.bluealsa_aplay, "--profile-sco"], stdout=PIPE, stderr=PIPE, shell=False)
+            self.aplay_sco = Popen([self.bluealsa_aplay, "--profile-sco"], stdout=PIPE, stderr=PIPE, shell=True)
 
         # Terminate aplay and arecord if already running
         if self.aplay_mic is not None:
@@ -49,5 +49,5 @@ class audio_route:
             self.arec_mic = None
 
         # Pipe Arecord output to Aplay to send over the SCO link
-        self.arec_mic = Popen([self.arecord, "-D plughw:1 -f S16_LE -c 1 -r 16000 mic"], stdout=PIPE, shell=True)
-        self.aplay_mic = Popen([self.aplay, "-D bluealsa:SRV=org.bluealsa,DEV=88:9F:6F:22:BE:55,PROFILE=sco mic"], stdout=PIPE, stdin=self.arec_mic.stdout, shell=True)
+        self.arec_mic = Popen([self.arecord, "-D plughw:1 -f S16_LE -c 1 -r 16000 mic.wav"], stdout=PIPE, shell=True)
+        self.aplay_mic = Popen([self.aplay, "-D bluealsa:SRV=org.bluealsa,DEV="+self.device_id+",PROFILE=sco mic.wav"], stdout=PIPE, stdin=self.arec_mic.stdout, shell=True)

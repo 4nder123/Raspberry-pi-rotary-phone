9a2c1f32dc8063bdfb07e669da3593dabbeeb585
4nder123 <57728624+4nder123@users.noreply.github.com>
2023-07-26 23:55:05 +0300

    Add files via upload

---
+++ bluetooth.py
@@ -0,0 +1,87 @@
+import time
+import pexpect
+import subprocess
+import sys
+
+class bluetooth:
+     def __init__(self):
+        out = subprocess.check_output("rfkill unblock bluetooth", shell = True)
+        self.child = pexpect.spawn("bluetoothctl", echo = False)
+
+    def get_output(self, command, pause = 0):
+        """Run a command in bluetoothctl prompt, return output as a list of lines."""
+        self.child.send(command + "\n")
+        time.sleep(pause)
+        start_failed = self.child.expect(["bluetooth", pexpect.EOF])
+
+        if start_failed:
+            raise BluetoothctlError("Bluetoothctl failed after running " + command)
+
+        return self.child.before.split("\r\n")
+
+    def make_discoverable(self):
+        """Make device discoverable."""
+        try:
+            out = self.get_output("discoverable on")
+        except BluetoothctlError, e:
+            print(e)
+            return None
+
+    def parse_device_info(self, info_string):
+        """Parse a string corresponding to a device."""
+        device = {}
+        block_list = ["[\x1b[0;", "removed"]
+        string_valid = not any(keyword in info_string for keyword in block_list)
+
+        if string_valid:
+            try:
+                device_position = info_string.index("Device")
+            except ValueError:
+                pass
+            else:
+                if device_position > -1:
+                    attribute_list = info_string[device_position:].split(" ", 2)
+                    device = {
+                        "mac_address": attribute_list[1],
+                        "name": attribute_list[2]
+                    }
+
+        return device
+
+    def get_paired_devices(self):
+        """Return a list of tuples of paired devices."""
+        try:
+            out = self.get_output("paired-devices")
+        except BluetoothctlError, e:
+            print(e)
+            return None
+        else:
+            paired_devices = []
+            for line in out:
+                device = self.parse_device_info(line)
+                if device:
+                    paired_devices.append(device)
+
+            return paired_devices
+
+    def get_device_info(self, mac_address):
+        """Get device info by mac address."""
+        try:
+            out = self.get_output("info " + mac_address)
+        except BluetoothctlError, e:
+            print(e)
+            return None
+        else:
+            return out
+
+    def connect(self, mac_address):
+        """Try to connect to a device by mac address."""
+        try:
+            out = self.get_output("connect " + mac_address, 2)
+        except BluetoothctlError, e:
+            print(e)
+            return None
+        else:
+            res = self.child.expect(["Failed to connect", "Connection successful", pexpect.EOF])
+            success = True if res == 1 else False
+            return success
--- main.py
+++ main.py
@@ -1,90 +1,86 @@
 from handsfree import handsfree
-from subprocess import Popen, PIPE
 from routing import audio_route
+from bluetooth import bluetooth
 from threading import Thread
 from gpiozero import Button, Motor
 from time import sleep
 import time
 from dbus.mainloop.glib import DBusGMainLoop
 from gi.repository import GLib
-import dbus
 
-def ring(motor, hf):
-    while True:
-        while hf.get_calls_state() == "incoming":
-            t_end = time.time() + 2
-            while time.time() < t_end:
-                motor.forward()
-                sleep(0.025)
-                motor.backward()
-                sleep(0.025)
-            sleep(3)
-        else:
-            motor.stop()
+class rotaryphone:
+    def __init__(self):
+        self.hook = Button(2)
+        self.nr_tap = Button(3)
+        self.dial_switch = Button(4)
+        self.motor = Motor(forward=17, backward=27)
+        self.hf = handsfree()
+        self.route = audio_route()
+        self.ringer = Thread(target=self.ring, daemon=True)
+        self.dial_sound = Thread(target=route.dial_sound, daemon=True)
+        self.call_start = False
+        self.dial_pressed = False
+        
+    def ring(self):
+        while True:
+            while self.hf.get_calls_state() == "incoming":
+                t_end = time.time() + 2
+                while time.time() < t_end:
+                    self.motor.forward()
+                    sleep(0.025)
+                    self.motor.backward()
+                    sleep(0.025)
+                sleep(3)
+            else:
+                self.motor.stop()
+            sleep(1)
 
-def get_number(nr_tap, dial_switch, hook, route):
-    i = 0
-    nr = 0
-    nrid = ""
-    pressed = True
-    add = False
-    stop_sound = False
-    sleep(1)
-    t = Thread(target=route.dial_sound, daemon=True)
-    t.start()
-    while hook.is_pressed:
-        if i == 300 and nrid != "":
-            route.close_dial_sound()
-            t.join()
-            return nrid
-        if dial_switch.is_pressed:
-            i = 0
-            add = True
-            if not nr_tap.is_pressed and pressed:
-                pressed = False
-                nr+=1
-            elif nr_tap.is_pressed and not pressed:
-                pressed = True  
-        elif add:
-            add = False
-            if nr == 10:
-                nr = 0
-            nrid = nrid + str(nr)
-            nr = 0
-        i += 1
-        sleep(0.01)
-    else:
+    def get_number(self):
+        nr = 0
         nrid = ""
-        route.close_dial_sound()
-        t.join()
-        return nrid
-
+        sleep(1)
+        t_end = time.time() + 3
+        self.dial_sound.start()
+        while self.hook.is_pressed or time.time() < t_end:
+            if self.dial_switch.is_pressed:
+                t_end = time.time() + 3
+                if not self.nr_tap.is_pressed and self.dial_pressed:
+                    self.dial_pressed = False
+                    nr+=1
+                elif self.nr_tap.is_pressed and not self.dial_pressed:
+                    self.dial_pressed = True  
+            elif nr != 0:
+                if nr == 10:
+                    nr = 0
+                nrid = nrid + str(nr)
+                nr = 0
+        else:
+            if not self.hook.is_pressed:
+                nrid = ""
+            self.route.close_dial_sound()
+            self.dial_sound.join()
+            return nrid
+        
+    def run(self):
+        self.route.run()
+        self.ringer.start()
+        while True:
+            if self.hook.is_pressed and self.hf.is_calls() and not self.call_start:
+                self.hf.anwser_calls()
+                self.call_start = True
+            elif self.hook.is_pressed and not self.call_start and not self.hf.is_calls():
+                self.call_start = True
+                nr = self.get_number()
+                if nr != "":
+                    self.hf.dial_number(nr)
+            if not self.hook.is_pressed and self.call_start:
+                self.call_start = False
+                self.hf.hangup()
+    
 if __name__ == '__main__':
     DBusGMainLoop(set_as_default=True)
-    hf = handsfree()
-    route = audio_route()
-    route.run()
-    bus = dbus.SystemBus()
-    manager = dbus.Interface(bus.get_object('org.ofono', '/'), 'org.ofono.Manager')
-    hook = Button(2)
-    nr_tap = Button(3)
-    dial_switch = Button(4)
-    motor = Motor(forward=17, backward=27)
-    t = Thread(target=ring, args=(motor, hf), daemon=True)
-    t.start()
-    call_start = False
-    while True:
-        if hook.is_pressed and hf.is_calls() and not call_start:
-            hf.anwser_calls()
-            call_start = True
-        elif hook.is_pressed and not call_start and not hf.is_calls():
-            call_start = True
-            nr = get_number(nr_tap, dial_switch, hook, route)
-            if nr != "":
-                hf.dial_number(nr)
-        if not hook.is_pressed and call_start:
-            call_start = False
-            hf.hangup()
+    rp = rotaryphone()
+    rp.run()
     GLib.MainLoop().run()
     
 
